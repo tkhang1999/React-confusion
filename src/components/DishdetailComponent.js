fccb6b9ec000bb0c913cb4a88a2770d176a7266f
Khang Le <tkhang1999@gmail.com>
2020-04-20 18:17:51 +0800

    fix components to follow Single Page Application

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem, ListGroupItemText, ListGroupItemHeading } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, 
+    ListGroup, ListGroupItem, ListGroupItemText, ListGroupItemHeading, 
+    Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 function RenderDish({dish}) {
     return (
@@ -45,11 +49,28 @@ const DishDetail = (props) => {
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/home">Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            {props.selectedDish.name}
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.selectedDish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <RenderDish dish={props.selectedDish} />
                     </div>                    
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.selectedDish.comments} />
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             </div>
